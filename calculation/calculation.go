8bf7648124b2f6a7347bb0c941317cf65e27b658
clobee <cbindika@gmail.com>
2020-03-15 23:22:24 +0000

    Invites only customer at less than 100km of dublin office

--- calculation/calculation.go
+++ calculation/calculation.go
@@ -1,2 +1,15 @@
 package calculation
 
+import (
+	"github.com/umahmood/haversine"
+)
+
+const oLong = 53.339428
+const oLat = -6.257664
+
+func GetCoord(lat float64, lon float64) float64 {
+	r := haversine.Coord{Lat: lat, Lon: lon}
+	o := haversine.Coord{Lat: oLat, Lon: oLong}
+	_, km := haversine.Distance(r, o)
+	return km
+}
--- main.go
+++ main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/clobee/customer-list-go/calculation"
 	"github.com/clobee/customer-list-go/customer"
 	"github.com/clobee/customer-list-go/data"
+	"log"
+	"strconv"
 )
+
+const limitDistance = 100
 
 func main() {
 	content := data.ReadFile("fixtures/customers.txt")
 
+	selCustomers := make(map[int]customer.Customer)
+
 	for line := range content {
-		user := customer.GetInfo(line)
-		fmt.Printf("customer : '%v' (%d)\n", user.Name, user.UserID)
+		u := customer.GetInfo(line)
+
+		l, err := strconv.ParseFloat(u.Longitude, 64)
+		la, err := strconv.ParseFloat(u.Latitude, 64)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c := calculation.GetCoord(l, la)
+		if c < limitDistance {
+			selCustomers[u.UserID] = u
+		}
+	}
+
+	for key, value := range selCustomers {
+		fmt.Printf("%d : %s \n", key, value.Name)
 	}
 }
